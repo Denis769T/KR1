bb4a84c663c67c2cc0ceeb08091732330afa2d7c
Denis <denis-topalov@yandex.ru>
2023-06-24 14:51:45 +1100

    Data Input

--- KR1/Program.cs
+++ KR1/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿int n=Numbers("Введите количество элементов массива: ");
+int m = 0;
+string[] str = new string[n];
+for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите элемент массива №{0}:\r\n    ", i + 1);
+        str[i] = Console.ReadLine(); //Заполняем массив
+    }
+    
+Console.WriteLine("----------------------------------------------------");
+Console.WriteLine("Вы ввели следующие строки:");
+Console.WriteLine("----------------------------------------------------");
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("  "+ str[i]);
+}
+
+Console.WriteLine("----------------------------------------------------");
+
+
+
+
+int Numbers(string text)
+{
+    Console.Write(text);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
